5134f16f51b13873d16917afcd303796baea400a
aliyurekli32 <adamjesperblack@gmail.com>
2023-03-06 22:57:16 +0300

    update: sign in and sign up

---
+++ src/screens/SignUp/index.js
@@ -0,0 +1,20 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const SignUpScreen = ({ onSignUp }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Public Sign Up Screen</Text>
+      <Button title="Sign Up" onPress={onSignUp} />
+    </View>
+  );
+};
+
+export default SignUpScreen;
