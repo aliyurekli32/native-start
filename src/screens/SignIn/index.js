bce80c0121d6a55d899a2bc38c07752f23bc92fe
aliyurekli32 <adamjesperblack@gmail.com>
2023-03-06 22:44:05 +0300

    update:

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Button } from 'react-native';
-import { View, Text, StyleSheet } from 'react-native';
+
+
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
