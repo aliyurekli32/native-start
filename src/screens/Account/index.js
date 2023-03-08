eed56f3d6f0793923fb1e27baec3510a5c4df9d5
aliyurekli32 <adamjesperblack@gmail.com>
2023-03-08 14:15:54 +0300

    add: account page

---
+++ src/screens/Account/index.js
@@ -0,0 +1,19 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const Account = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Protected Account Screen</Text>
+    </View>
+  );
+};
+
+export default Account;
